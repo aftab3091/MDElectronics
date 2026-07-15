3d8d0a1e83840aa0ebad41a352f12b5d5dbca27d
aftab3091 <aftabali6896@gmail.com>
2026-07-15 12:12:25 +0530

    remove errors

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pip install -r requirements.txt
 python3 Backend/electronicstore/manage.py collectstatic --noinput --clear
+
+# Copy media files into staticfiles directory so Vercel can deploy and serve them as static assets
+cp -r media staticfiles/
+
